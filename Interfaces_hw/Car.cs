d93dae8acbe1fe22b6806f2dd5560dcfbd8b7ac9
Fatima <fatimavaliyeva0704@gmail.com>
2024-08-29 14:32:11 +0400

    Add project files.

---
+++ Interfaces_hw/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_hw
+{
+    public class Car : IComparable<Car>, IEquatable<Car>, ICloneable
+    {
+        public string Name { get; set; }
+        public int MaxMph { get; set; }
+        public int Horsepower { get; set; }
+        public decimal Price { get; set; }
+
+        public Car(string name, int maxmph, int hp, decimal price)
+        {
+            Name = name;
+            MaxMph = maxmph;
+            Horsepower = hp;
+            Price = price;
+        }
+
+        public object Clone()
+        {
+            var car = new Car(Name,MaxMph,Horsepower,Price);
+            car.Name = Name;
+            car.MaxMph = MaxMph;
+            car.Horsepower = Horsepower;
+            car.Price = Price;
+
+            return car;
+        }
+
+        public int CompareTo(Car? other)
+        {
+            if (other == null)
+                throw new ArgumentException("Is null");
+
+            return this.Name.CompareTo(other.Name);
+
+        }
+
+        public bool Equals(Car? other)
+        {
+           return Name == other.Name && MaxMph == other.MaxMph && Horsepower == other.Horsepower && Price == other.Price;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} - MaxMph: {MaxMph} - Horse Power: {Horsepower} - Price: ${Price}";
+        }
+    }
+}
---
+++ Interfaces_hw/Program.cs
@@ -0,0 +1,28 @@
+﻿using Interfaces_hw;
+
+List<Car> cars = new List<Car> { new("Toyota Highlander", 150, 200, 100000m), new("Lamborghini Urus", 200, 450, 203244) };
+
+Car ogCar = new("Mitsubishi L200", 150, 210, 68000);
+
+cars.Sort();
+
+Console.WriteLine("Here are cars sorted by their name:");
+foreach (Car car in cars)
+{
+    Console.WriteLine(car);
+}
+
+
+Car clonedCar = (Car)ogCar.Clone();
+Console.WriteLine($"\n\nOriginal car: {ogCar}\nCloned car: {clonedCar}");
+
+
+Car car1 = new("Toyota Fielder", 100, 150, 30000);
+Car car2 = new("Nissan GT-R", 199, 600, 210000);
+
+Console.WriteLine("\n\n");
+Console.WriteLine(car1);
+Console.WriteLine(car2);
+
+Console.WriteLine("\nSee if its equal");
+Console.WriteLine($"Equal?: {car1.Equals(car2)}");
